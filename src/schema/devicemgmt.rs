605a023388f10923e3d09d87dcc097a70c2dee46
Dmitry Samoylov <dmitry.samoylov@quantumsoft.ru>
2019-12-11 19:31:22 +0700

    Add sketches of request-response operations

--- src/schema/devicemgmt.rs
+++ src/schema/devicemgmt.rs
@@ -1,73 +1,224 @@
-pub mod get_system_date_and_time_request {
-    use std::io::Write;
-    use yaserde::YaSerialize;
+// This file is an example of how generated code will look like. It contains some comments
+// to see the relation between xml's and Rust code.
 
-    #[derive(Default, PartialEq, Debug, YaSerialize)]
-    #[yaserde(
-    root = "Envelope",
-    prefix = "s",
-    namespace = "s: http://www.w3.org/2003/05/soap-envelope",
-    namespace = "tds: http://www.onvif.org/ver10/device/wsdl",
-    namespace = "tt: http://www.onvif.org/ver10/schema",
-    )]
-    pub struct Envelope {
-        #[yaserde(prefix = "s", rename = "Body")]
-        body: Body,
-    }
+// Quote from ONVIF core spec (5.5 messages):
+//
+//    The  WSDL  message  part  element  is  used  to  define  the  actual  format  of  the  message.
+//    Although there can be multiple parts in a WSDL message, this specification follows the WS-I basic
+//    profile  [WS-I  BP  2.0]  and  does  not  allow  more  than  one  part  element  in  a  message.
+//    Hence we always use the same name (“parameters”) for the message part name. The following WSDL
+//    notation is used for ONVIF specifications:
+//
+//    <message name="’Operation_Name’Request”>
+//       <part name="parameters" element="’prefix’:’Operation_Name’"/>
+//    </message>
+//
+//    respective,
+//    <message name="’Operation_Name’Response”>
+//        <part name="parameters" element="’prefix’:’Operation_Name’Response”/>
+//    </message>
+//
+//    where 'prefix' is the prefix for the namespace in which the message is defined.
+//
+//
+// So according to the spec we will use only single-part messages.
 
-    #[derive(Default, PartialEq, Debug, YaSerialize)]
-    #[yaserde(
-    prefix = "s",
-    namespace = "s: http://www.w3.org/2003/05/soap-envelope",
-    )]
-    pub struct Body {
-        #[yaserde(prefix = "tds", rename = "GetSystemDateAndTime")]
-        get_system_date_and_time: GetSystemDateAndTime,
-    }
 
-    #[derive(Default, PartialEq, Debug, YaSerialize)]
-    #[yaserde(
-    prefix = "tds",
-    namespace = "tds: http://www.onvif.org/ver10/device/wsdl"
-    )]
-    pub struct GetSystemDateAndTime {
-    }
+// Quote from ONVIF core spec (5.6.2 One-way operation type):
+//     <operation name=”’Operation_Name’”>
+//         <input message=”’prefix’:’Operation_Name’”/>
+//     </operation>
+//
+// Quote from ONVIF core spec (5.6.3 Request-response operation type):
+//     <operation name=”’Operation_Name’”>
+//         <input message=”’prefix’:’Operation_Name’”/>
+//         <output message=”’prefix’:’Operation_Name’Response”/>
+//         <fault name> = “Fault” message = “’prefix’:’FaultMessage_Name’”>
+//     </operation>
+//
+//
+// So we have 2 types of operations, some with fault message.
+// But after some grepping over WSDLs we can state:
+//  - Only event.wsdl has one-way operations.
+//  - Only event.wsdl has operations with fault messages.
+//  - All other wsdl's except event.wsdl (25 in total) have only req-rep operations
+//     with exactly 1 input and 1 output message.
+//
+// For now it looks like we can divide all files into 2 categories:
+//  - event.wsdl. It is a little bit more complex case for code generation.
+//  - everything else. Simpler case. Let current file (devicegmt.rs) be an example of this group.
+
+
+use std::io::{Read, Write};
+use yaserde::{YaSerialize, YaDeserialize};
+use crate::schema::onvif as tt;
+
+
+
+//    <xs:element name="GetSystemDateAndTime">
+//        <xs:complexType>
+//            <xs:sequence/>
+//        </xs:complexType>
+//    </xs:element>
+
+#[derive(Default, PartialEq, Debug, YaSerialize)]
+#[yaserde(
+prefix = "tds",
+namespace = "tds: http://www.onvif.org/ver10/device/wsdl"
+)]
+pub struct GetSystemDateAndTime {
 }
 
 
-pub mod get_system_date_and_time_response {
-    use std::io::Read;
-    use yaserde::YaDeserialize;
-    use crate::schema::onvif as tt;
+//    <xs:element name="GetSystemDateAndTimeResponse">
+//        <xs:complexType>
+//            <xs:sequence>
+//                <xs:element name="SystemDateAndTime" type="tt:SystemDateTime">
+//                    <xs:annotation>
+//                        <xs:documentation>Contains information whether system date and time are set manually or by NTP, daylight savings is on or off, time zone in POSIX 1003.1 format and system date and time in UTC and also local system date and time.</xs:documentation>
+//                    </xs:annotation>
+//                </xs:element>
+//            </xs:sequence>
+//        </xs:complexType>
+//    </xs:element>
 
-    #[derive(Default, PartialEq, Debug, YaDeserialize)]
-    #[yaserde(
-    root = "Envelope",
-    prefix = "s",
-    namespace = "s: http://www.w3.org/2003/05/soap-envelope"
-    )]
-    pub struct Envelope {
-        #[yaserde(rename = "Body")]
-        pub body: Body,
-    }
+// Contains information whether system date and time are set manually or by NTP,
+// daylight savings is on or off, time zone in POSIX 1003.1 format and system date
+// and time in UTC and also local system date and time.
+#[derive(Default, PartialEq, Debug, YaDeserialize)]
+#[yaserde(
+prefix = "tds",
+namespace = "tds: http://www.onvif.org/ver10/device/wsdl"
+)]
+pub struct GetSystemDateAndTimeResponse {
+    #[yaserde(rename = "SystemDateAndTime")]
+    pub system_date_and_time: tt::SystemDateAndTime,
+}
 
-    #[derive(Default, PartialEq, Debug, YaDeserialize)]
-    #[yaserde(
-    prefix = "s",
-    namespace = "s: http://www.w3.org/2003/05/soap-envelope"
-    )]
-    pub struct Body {
-        #[yaserde(rename = "GetSystemDateAndTimeResponse")]
-        pub get_system_date_and_time_response: GetSystemDateAndTimeResponse,
-    }
 
-    #[derive(Default, PartialEq, Debug, YaDeserialize)]
-    #[yaserde(
-    prefix = "tds",
-    namespace = "tds: http://www.onvif.org/ver10/device/wsdl"
-    )]
-    pub struct GetSystemDateAndTimeResponse {
-        #[yaserde(rename = "SystemDateAndTime")]
-        pub system_date_and_time: tt::SystemDateAndTime,
-    }
+//    <wsdl:operation name="GetSystemDateAndTime">
+//        <wsdl:documentation>This operation gets the device system date and time. The device shall support the return of
+//            the daylight saving setting and of the manual system date and time (if applicable) or indication
+//            of NTP time (if applicable) through the GetSystemDateAndTime command.<br/>
+//            A device shall provide the UTCDateTime information.
+//        </wsdl:documentation>
+//        <wsdl:input message="tds:GetSystemDateAndTimeRequest"/>
+//        <wsdl:output message="tds:GetSystemDateAndTimeResponse"/>
+//    </wsdl:operation>
+//
+//    <wsdl:message name="GetSystemDateAndTimeRequest">
+//        <wsdl:part name="parameters" element="tds:GetSystemDateAndTime"/>
+//    </wsdl:message>
+//
+//    <wsdl:message name="GetSystemDateAndTimeResponse">
+//        <wsdl:part name="parameters" element="tds:GetSystemDateAndTimeResponse"/>
+//    </wsdl:message>
+
+
+// This operation gets the device system date and time. The device shall support the return of
+// the daylight saving setting and of the manual system date and time (if applicable) or
+// indication of NTP time (if applicable) through the GetSystemDateAndTime command.
+// A device shall provide the UTCDateTime information.
+
+pub fn get_system_date_and_time(_request: &GetSystemDateAndTime) -> Option<GetSystemDateAndTimeResponse> {
+    unimplemented!()
+}
+
+
+
+
+
+//    <xs:element name="GetDeviceInformation">
+//        <xs:complexType>
+//            <xs:sequence/>
+//        </xs:complexType>
+//    </xs:element>
+
+#[derive(Default, PartialEq, Debug, YaSerialize)]
+#[yaserde(
+prefix = "tds",
+namespace = "tds: http://www.onvif.org/ver10/device/wsdl"
+)]
+pub struct GetDeviceInformation {
+}
+
+
+//    <xs:element name="GetDeviceInformationResponse">
+//        <xs:complexType>
+//            <xs:sequence>
+//                <xs:element name="Manufacturer" type="xs:string">
+//                    <xs:annotation>
+//                        <xs:documentation>The manufactor of the device.</xs:documentation>
+//                    </xs:annotation>
+//                </xs:element>
+//                <xs:element name="Model" type="xs:string">
+//                    <xs:annotation>
+//                        <xs:documentation>The device model.</xs:documentation>
+//                    </xs:annotation>
+//                </xs:element>
+//                <xs:element name="FirmwareVersion" type="xs:string">
+//                    <xs:annotation>
+//                        <xs:documentation>The firmware version in the device.</xs:documentation>
+//                    </xs:annotation>
+//                </xs:element>
+//                <xs:element name="SerialNumber" type="xs:string">
+//                    <xs:annotation>
+//                        <xs:documentation>The serial number of the device.</xs:documentation>
+//                    </xs:annotation>
+//                </xs:element>
+//                <xs:element name="HardwareId" type="xs:string">
+//                    <xs:annotation>
+//                        <xs:documentation>The hardware ID of the device.</xs:documentation>
+//                    </xs:annotation>
+//                </xs:element>
+//            </xs:sequence>
+//        </xs:complexType>
+//    </xs:element>
+
+
+#[derive(Default, PartialEq, Debug, YaDeserialize)]
+#[yaserde(
+prefix = "tds",
+namespace = "tds: http://www.onvif.org/ver10/device/wsdl"
+)]
+pub struct GetDeviceInformationResponse {
+    // The manufactor of the device.
+    #[yaserde(rename = "Manufacturer")]
+    pub manufacturer: String,
+
+    // The device model.
+    #[yaserde(rename = "Model")]
+    pub model: String,
+
+    // The firmware version in the device.
+    #[yaserde(rename = "FirmwareVersion")]
+    pub firmware_version: String,
+
+    // The serial number of the device.
+    #[yaserde(rename = "SerialNumber")]
+    pub serial_number: String,
+
+    // The hardware ID of the device.
+    #[yaserde(rename = "HardwareId")]
+    pub hardware_id: String,
+}
+
+
+//    <wsdl:operation name="GetDeviceInformation">
+//        <wsdl:documentation>This operation gets basic device information from the device.</wsdl:documentation>
+//        <wsdl:input message="tds:GetDeviceInformationRequest"/>
+//        <wsdl:output message="tds:GetDeviceInformationResponse"/>
+//    </wsdl:operation>
+//
+//    <wsdl:message name="GetDeviceInformationRequest">
+//        <wsdl:part name="parameters" element="tds:GetDeviceInformation"/>
+//    </wsdl:message>
+//
+//    <wsdl:message name="GetDeviceInformationResponse">
+//        <wsdl:part name="parameters" element="tds:GetDeviceInformationResponse"/>
+//    </wsdl:message>
+
+// This operation gets basic device information from the device.
+pub fn get_device_information(_request: &GetDeviceInformation) -> Option<GetDeviceInformationResponse> {
+    unimplemented!()
 }
--- src/schema/tests.rs
+++ src/schema/tests.rs
@@ -8,31 +8,9 @@ use onvif as tt;
 fn basic_deserialization() {
     let response = r#"
     <?xml version="1.0" encoding="UTF-8"?>
-    <s:Envelope xmlns:s="http://www.w3.org/2003/05/soap-envelope" xmlns:e="http://www.w3.org/2003/05/soap-encoding"
-                xmlns:wsa="http://www.w3.org/2005/08/addressing" xmlns:xs="http://www.w3.org/2001/XMLSchema"
-                xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:wsaw="http://www.w3.org/2006/05/addressing/wsdl"
-                xmlns:wsnt="http://docs.oasis-open.org/wsn/b-2" xmlns:wstop="http://docs.oasis-open.org/wsn/t-1"
-                xmlns:wsntw="http://docs.oasis-open.org/wsn/bw-2" xmlns:wsrf-rw="http://docs.oasis-open.org/wsrf/rw-2"
-                xmlns:wsrf-r="http://docs.oasis-open.org/wsrf/r-2" xmlns:wsrf-bf="http://docs.oasis-open.org/wsrf/bf-2"
-                xmlns:wsdl="http://schemas.xmlsoap.org/wsdl" xmlns:wsoap12="http://schemas.xmlsoap.org/wsdl/soap12"
-                xmlns:http="http://schemas.xmlsoap.org/wsdl/http" xmlns:d="http://schemas.xmlsoap.org/ws/2005/04/discovery"
-                xmlns:wsadis="http://schemas.xmlsoap.org/ws/2004/08/addressing" xmlns:tt="http://www.onvif.org/ver10/schema"
-                xmlns:tns1="http://www.onvif.org/ver10/topics" xmlns:tds="http://www.onvif.org/ver10/device/wsdl"
-                xmlns:trt="http://www.onvif.org/ver10/media/wsdl" xmlns:tev="http://www.onvif.org/ver10/events/wsdl"
-                xmlns:timg="http://www.onvif.org/ver20/imaging/wsdl" xmlns:tst="http://www.onvif.org/ver10/storage/wsdl"
-                xmlns:dn="http://www.onvif.org/ver10/network/wsdl" xmlns:tr2="http://www.onvif.org/ver20/media/wsdl"
-                xmlns:tptz="http://www.onvif.org/ver20/ptz/wsdl" xmlns:tan="http://www.onvif.org/ver20/analytics/wsdl"
-                xmlns:axt="http://www.onvif.org/ver20/analytics" xmlns:trp="http://www.onvif.org/ver10/replay/wsdl"
-                xmlns:tse="http://www.onvif.org/ver10/search/wsdl" xmlns:trc="http://www.onvif.org/ver10/recording/wsdl"
-                xmlns:tac="http://www.onvif.org/ver10/accesscontrol/wsdl"
-                xmlns:tdc="http://www.onvif.org/ver10/doorcontrol/wsdl" xmlns:pt="http://www.onvif.org/ver10/pacs"
-                xmlns:tmd="http://www.onvif.org/ver10/deviceIO/wsdl" xmlns:tth="http://www.onvif.org/ver10/thermal/wsdl"
-                xmlns:tcr="http://www.onvif.org/ver10/credential/wsdl"
-                xmlns:tar="http://www.onvif.org/ver10/accessrules/wsdl" xmlns:tsc="http://www.onvif.org/ver10/schedule/wsdl"
-                xmlns:trv="http://www.onvif.org/ver10/receiver/wsdl"
-                xmlns:tpv="http://www.onvif.org/ver10/provisioning/wsdl" xmlns:ter="http://www.onvif.org/ver10/error">
-        <s:Body>
-            <tds:GetSystemDateAndTimeResponse>
+            <tds:GetSystemDateAndTimeResponse
+                xmlns:tt="http://www.onvif.org/ver10/schema"
+                xmlns:tds="http://www.onvif.org/ver10/device/wsdl">
                 <tds:SystemDateAndTime>
                     <tt:DateTimeType>NTP</tt:DateTimeType>
                     <tt:DaylightSavings>false</tt:DaylightSavings>
@@ -53,15 +31,11 @@ fn basic_deserialization() {
                     </tt:UTCDateTime>
                 </tds:SystemDateAndTime>
             </tds:GetSystemDateAndTimeResponse>
-        </s:Body>
-    </s:Envelope>
     "#;
 
 
-    let envelope: devicemgmt::get_system_date_and_time_response::Envelope = yaserde::de::from_str(&response).unwrap();
-    let system_date_and_time = envelope.body
-        .get_system_date_and_time_response
-        .system_date_and_time;
+    let response: devicemgmt::GetSystemDateAndTimeResponse = yaserde::de::from_str(&response).unwrap();
+    let system_date_and_time = response.system_date_and_time;
 
     println!("{:#?}", system_date_and_time);
 
@@ -80,18 +54,11 @@ fn basic_deserialization() {
 #[test]
 fn basic_serialization() {
     let expected = r#"<?xml version="1.0" encoding="UTF-8"?>
-    <s:Envelope
-        xmlns:s="http://www.w3.org/2003/05/soap-envelope"
-        xmlns:tds="http://www.onvif.org/ver10/device/wsdl"
-        xmlns:tt="http://www.onvif.org/ver10/schema">
-        <s:Body>
-            <tds:GetSystemDateAndTime/>
-        </s:Body>
-    </s:Envelope>
+            <tds:GetSystemDateAndTime xmlns:tds="http://www.onvif.org/ver10/device/wsdl" />
     "#;
 
-    let envelope: devicemgmt::get_system_date_and_time_request::Envelope = Default::default();
-    let actual = yaserde::ser::to_string(&envelope).unwrap();
+    let request: devicemgmt::GetSystemDateAndTime = Default::default();
+    let actual = yaserde::ser::to_string(&request).unwrap();
 
     let actual_iter = xml::EventReader::new(actual.as_bytes())
         .into_iter()
