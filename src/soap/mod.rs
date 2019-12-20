ed50682636b59d114825dbec23d4bf55122ed5a5
Dmitry Samoylov <dmitry.samoylov@quantumsoft.ru>
2019-12-20 16:42:34 +0700

    Rewrite soap module with more appropriate xmltree

--- src/soap/mod.rs
+++ src/soap/mod.rs
@@ -1,67 +1,75 @@
 const SOAP_URI: &str = "http://www.w3.org/2003/05/soap-envelope";
 
-pub fn soap(xml: &str) -> Option<String> {
-    let mut out = vec![];
-
-    let mut writer = xml::EmitterConfig::new().create_writer(&mut out);
-
-    let reader = xml::EventReader::new(xml.as_bytes());
-
-    writer
-        .write(xml::writer::XmlEvent::start_element("s:Envelope").ns("s", SOAP_URI))
-        .unwrap();
-    writer
-        .write(xml::writer::XmlEvent::start_element("s:Body").ns("s", SOAP_URI))
-        .unwrap();
-
-    reader
-        .into_iter()
-        .filter(|x| match x {
-            Ok(xml::reader::XmlEvent::StartDocument { .. }) => false,
-            _ => true,
-        })
-        .for_each(|event| {
-            if let Ok(event) = event {
-                if let Some(event) = event.as_writer_event() {
-                    writer.write(event).unwrap();
-                }
-            }
-        });
-
-    writer.write(xml::writer::XmlEvent::end_element()).unwrap();
-    writer.write(xml::writer::XmlEvent::end_element()).unwrap();
-
-    String::from_utf8(out).ok()
+#[derive(Debug)]
+pub enum Error {
+    ParseError,
+    EnvelopeNotFound,
+    BodyNotFound,
+    BodyIsEmpty,
 }
 
-pub fn unsoap(xml: &str) -> Option<String> {
-    let reader = xml::EventReader::new(xml.as_bytes());
+#[derive(Debug)]
+pub struct Response {
+    response: Option<String>,
+    fault: Option<Fault>,
+}
 
-    let mut out = vec![];
-
-    let mut writer = xml::EmitterConfig::new().create_writer(&mut out);
-
-    let is_soap = |name: &xml::name::OwnedName| {
-        name.namespace == Some(SOAP_URI.to_string())
-            && (name.local_name == "Envelope" || name.local_name == "Body")
+pub fn soap(xml: &str) -> Option<String> {
+    let app_data = match xmltree::Element::parse(xml.as_bytes()) {
+        Ok(v) => v,
+        Err(_) => return None,
     };
 
-    reader
-        .into_iter()
-        .filter(|x| match x {
-            Ok(xml::reader::XmlEvent::StartElement { name, .. }) => !is_soap(&name),
-            Ok(xml::reader::XmlEvent::EndElement { name }) => !is_soap(&name),
-            _ => true,
-        })
-        .for_each(|event| {
-            if let Ok(event) = event {
-                if let Some(event) = event.as_writer_event() {
-                    writer.write(event).unwrap();
-                }
-            }
-        });
+    let mut namespaces = app_data
+        .namespaces
+        .clone()
+        .unwrap_or(xmltree::Namespace::empty());
+    namespaces.put("s", SOAP_URI);
 
-    String::from_utf8(out).ok()
+    let mut body = xmltree::Element::new("Body");
+    body.prefix = Some("s".to_string());
+    body.children.push(app_data);
+
+    let mut envelope = xmltree::Element::new("Envelope");
+    envelope.namespaces = Some(namespaces);
+    envelope.prefix = Some("s".to_string());
+    envelope.children.push(body);
+
+    Some(xml_element_to_string(&envelope))
+}
+
+pub fn unsoap(xml: &str) -> Result<Response, Error> {
+    let envelope = match xmltree::Element::parse(xml.as_bytes()) {
+        Ok(envelope) => match envelope.name.as_ref() {
+            "Envelope" => Ok(envelope),
+            _ => Err(Error::EnvelopeNotFound),
+        },
+        _ => Err(Error::ParseError),
+    }?;
+
+    let body = match envelope.get_child("Body") {
+        Some(body) => Ok(body),
+        None => Err(Error::BodyNotFound),
+    }?;
+
+    let response = match body.children.first() {
+        Some(app_data) => Ok(xml_element_to_string(&app_data)),
+        _ => Err(Error::BodyIsEmpty),
+    }?;
+
+    let fault = body.get_child("Fault").and_then(|elem| get_fault(elem));
+
+    Ok(Response {
+        response: Some(response),
+        fault,
+    })
+}
+
+fn xml_element_to_string(el: &xmltree::Element) -> String {
+    // TODO: process errors
+    let mut out = vec![];
+    el.write(&mut out).unwrap();
+    String::from_utf8(out).unwrap()
 }
 
 #[cfg(test)]
@@ -79,10 +87,11 @@ mod tests {
         "#;
 
         let expected = r#"
-            <?xml version="1.0" encoding="utf-8"?>
-            <s:Envelope xmlns:s="http://www.w3.org/2003/05/soap-envelope">
+            <?xml version="1.0" encoding="UTF-8"?>
+            <s:Envelope xmlns:s="http://www.w3.org/2003/05/soap-envelope"
+                        xmlns:my="http://www.example.my/schema">
                 <s:Body>
-                    <my:Book xmlns:my="http://www.example.my/schema">
+                    <my:Book>
                         <my:Title>Such book</my:Title>
                         <my:Pages>42</my:Pages>
                     </my:Book>
@@ -148,11 +157,19 @@ mod tests {
 
         let actual = unsoap(input).unwrap();
 
-        println!("{}", actual);
+        println!("{:?}", actual);
 
-        let parsed: Book = yaserde::de::from_str(&actual).unwrap();
+        let parsed: Book = yaserde::de::from_str(&actual.response.unwrap()).unwrap();
 
         assert_eq!(parsed.title, "Such book");
         assert_eq!(parsed.pages, 42);
     }
+}
+
+// TODO: process faults
+#[derive(Debug)]
+pub struct Fault;
+
+fn get_fault(_envelope: &xmltree::Element) -> Option<Fault> {
+    None
 }
