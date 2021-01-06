043c6792904001b8a70dc17598b44c45a7834902
Jonas Platte <jonas@lumeo.com>
2021-01-06 18:00:22 +0100

    Use unnamed lifetime in xml_eq::without_spaces

--- schema/src/utils/xml_eq.rs
+++ schema/src/utils/xml_eq.rs
@@ -4,9 +4,9 @@ pub fn assert_xml_eq(actual: &str, expected: &str) {
     }
 }
 
-fn without_whitespaces<'a>(
-    expected: &'a str,
-) -> impl Iterator<Item = Result<xml::reader::XmlEvent, xml::reader::Error>> + 'a {
+fn without_whitespaces(
+    expected: &str,
+) -> impl Iterator<Item = Result<xml::reader::XmlEvent, xml::reader::Error>> + '_ {
     xml::EventReader::new(expected.as_bytes())
         .into_iter()
         .filter(|e| !matches!(e, Ok(xml::reader::XmlEvent::Whitespace(_))))
