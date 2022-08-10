cb5bd9664fc952fb8178be6a44b8c0ee5e683506
Dmitry Samoylov <dmitry.samoylov@quantumsoft.ru>
2022-08-10 17:42:30 +0700

    Add feature gates to most modules
    This allows to speedup compilation by disabling default
    features and enabling only necessary ones.

--- schema/src/lib.rs
+++ schema/src/lib.rs
@@ -11,10 +11,13 @@ pub mod validate;
 
 // xsd:
 pub mod common;
+#[cfg(feature = "metadatastream")]
 #[allow(clippy::large_enum_variant)]
 pub mod metadatastream;
 pub mod onvif;
+#[cfg(feature = "radiometry")]
 pub mod radiometry;
+#[cfg(feature = "rules")]
 pub mod rules;
 pub mod soap_envelope;
 pub mod types;
@@ -22,32 +25,57 @@ pub mod xmlmime;
 pub mod xop;
 
 // wsdl:
+#[cfg(feature = "accesscontrol")]
 pub mod accesscontrol;
+#[cfg(feature = "accessrules")]
 pub mod accessrules;
+#[cfg(feature = "actionengine")]
 pub mod actionengine;
+#[cfg(feature = "advancedsecurity")]
 pub mod advancedsecurity;
+#[cfg(feature = "analytics")]
 pub mod analytics;
+#[cfg(feature = "authenticationbehavior")]
 pub mod authenticationbehavior;
 pub mod b_2;
+#[cfg(feature = "bf_2")]
 pub mod bf_2;
+#[cfg(feature = "credential")]
 pub mod credential;
+#[cfg(feature = "deviceio")]
 pub mod deviceio;
+#[cfg(feature = "devicemgmt")]
 pub mod devicemgmt;
+#[cfg(feature = "display")]
 pub mod display;
+#[cfg(feature = "doorcontrol")]
 pub mod doorcontrol;
+#[cfg(feature = "event")]
 pub mod event;
+#[cfg(feature = "imaging")]
 pub mod imaging;
+#[cfg(feature = "media")]
 pub mod media;
+#[cfg(feature = "media2")]
 pub mod media2;
+#[cfg(feature = "provisioning")]
 pub mod provisioning;
+#[cfg(feature = "ptz")]
 pub mod ptz;
+#[cfg(feature = "receiver")]
 pub mod receiver;
+#[cfg(feature = "recording")]
 pub mod recording;
+#[cfg(feature = "replay")]
 pub mod replay;
+#[cfg(feature = "schedule")]
 pub mod schedule;
+#[cfg(feature = "search")]
 pub mod search;
 pub mod t_1;
+#[cfg(feature = "thermal")]
 pub mod thermal;
+#[cfg(feature = "uplink")]
 pub mod uplink;
 pub mod ws_addr;
 pub mod ws_discovery;
